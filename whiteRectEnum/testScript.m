3c9019be661e7b19d1518d60678964e283bc1aa0
Terry Kong <terrycurtiskong@gmail.com>
2015-05-28 14:08:51 -0700

    resolved conflict
    resolved conflict w/ whiteRectEnum test_script

--- whiteRectEnum/testScript.m
+++ whiteRectEnum/testScript.m
@@ -1,5 +1,7 @@
-<<<<<<< Updated upstream
+cd ..
 imInit = imread('testIm/001.jpg');
+cd whiteRectEnum
+
 n = 1165;
 im = ~im2bw(imInit(1125:min(n,end),1125:min(n,end)),0.5);
 
@@ -8,13 +10,7 @@ set(0,'RecursionLimit',500);
 %%
 A = [im,ones(size(im,1),1)];
 
-=======
-A = [1,0,1;...
-     1,0,1;...
-     0,0,1];
-A = repmat(A,2,2);
-figure; imagesc(A); colormap('gray')
->>>>>>> Stashed changes
+
 fst = RectangleNode(0,0,size(A,1));
 tree = RectangleTree(fst);
 

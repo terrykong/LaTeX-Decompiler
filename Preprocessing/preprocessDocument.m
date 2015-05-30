668cd38090c3ad62d1a62d055c071315eacf29e8
mwieman <mwieman@stanford.edu>
2015-05-30 00:50:03 -0700

    Whoops, wrong file before
    My bad... slightly different code for margin removal

--- Preprocessing/preprocessDocument.m
+++ Preprocessing/preprocessDocument.m
@@ -2,17 +2,20 @@ function [ processed_image ] = preprocessDocument( input_image )
 % Preprocesses image of text document
 % input: n-m
 % Binarizes image
-% - uses _ method
-% - local vs global
+% - uses locally adaptive thresholding
+% - sets all low variance locations to 1
 % Determines and corrects skew
-% - to within __ degrees
-% - uses __ method
-% - slant detection? 
-% rebinarizes after correcting for skew? 
+% - for angles within the range -45<theta<45
+% - uses Hough transform on smaller blocks of the image
 % Removes margin
+% - projects pixels onto x/y axes and detects where the peaks from text
+% occur
+% Maybe: slant detection, homography of image?
 
 input_image = rgb2gray(im2double(input_image));
-
+binarized_image = binarizeDocument(input_image);
+rotated_image = determineBlockSkew(binarized_image);
+processed_image = findPageMargins(rotated_image);
 
 end
 

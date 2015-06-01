e6e0a04b771d4aa6362d44272e5042d559ab8da5
DasVinch <vincent.deo.11@gmail.com>
2015-06-01 09:27:02 -0700

    MWR call change in testScript for downsampling correctness

--- whiteRectEnum/testScript.m
+++ whiteRectEnum/testScript.m
@@ -5,52 +5,33 @@ n = 1700;
 im = im2bw(imInit);
 [im, componentLocation] = outlineConnectedComponents(im);
 
-set(0,'RecursionLimit',500);
-
+size(im)
+im = im(1:2407,1:2502);
 [imTopLeft,imTop,mask,boundingBox,CCLoc,downFactor] = oCCReduce(im);
-im = ~imTopLeft;
+downFactor
+imReduced = ~imTopLeft;
+size(imReduced)
 
 %% Run those 2 lines if not done yet. If multiple time no worries
 javaaddpath('../whiteRectEnumJava/bin'); % assuming your matlab workspace is currently /LaTeX-Decompiler/whiterectEnum
 import whiteRectEnumJava.*
+set(0,'RecursionLimit',500);
 
 %%
-%     A = [im,ones(size(im,1),1)];
-%     tic;
-%
-%     fst = RectangleNode(0,0,size(A,1));
-%     tree = RectangleTree(fst);
-%
-%     nodeList = RectangleList([],[]);
-%
-%     [row,col] = find(A);
-%     for k = 1:numel(row)
-%         i = row(k);
-%         j = col(k);
-%         tree = tree.processPoint(nodeList,j-0.5,i-0.5,0,size(A,1));
-%         % tree.debugCycles
-%         %             tree.print();
-%         %             input([num2str(i),' ',num2str(j)]);
-%     end
-%     list = nodeList.print()
-%     toc;
-
-
-%%
-A = [im,ones(size(im,1),1)];
+A = [imReduced,ones(size(imReduced,1),1)];
 
 [row,col] = find(A);
 tic
-list = MatlabComp.processImage(row,col,size(A,1))';
+list = (MatlabComp.processImage(row,col,size(A,1))' - 1)*downFactor + 1;
 toc
 
 %%
 % figure(1)
 % imagesc(A,[0 1]); axis('image'); colorbar;
 % 
-B = A*0.5+0.5;
+B = im*0.5+0.5;
 for rect = list
-    B = A*0.5+0.5;
+    B = im*0.5+0.5;
     B(rect(1):rect(2),rect(3):rect(4)) = 0;
     imagesc(B,[0 1]); axis('image'); colorbar;
     rect
